20208c9dbe7f71df46b533ef81619d296ad6d2e6
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-01-14 08:54:53 +0100

    this

--- tests/integration_tests/tests/agentless_tests/test_resume.py
+++ tests/integration_tests/tests/agentless_tests/test_resume.py
@@ -18,92 +18,74 @@ from integration_tests import AgentlessTestCase
 from integration_tests.tests.utils import get_resource as resource
 
 
-class TestTaskResume(AgentlessTestCase):
+class TestResumeMgmtworker(AgentlessTestCase):
+    wait_message = 'WAITING FOR FILE'
+    target_file = '/tmp/continue_test'
 
-    def test_resumable_mgmtworker_op(self):
-        wait_message = 'WAITING FOR FILE'
-        target_file = '/tmp/continue_test'
+    def _create_deployment(self):
         dsl_path = resource("dsl/resumable_mgmtworker.yaml")
-        deployment = self.deploy(dsl_path)
-        instances = self.client.node_instances.list(
-            deployment_id=deployment.id)
-        instance_id = instances[0].id
+        return self.deploy(dsl_path)
 
-        execution = self.execute_workflow(
+    def _start_execution(self, deployment, operation):
+        return self.execute_workflow(
             workflow_name='execute_operation',
             wait_for_execution=False,
             deployment_id=deployment.id,
-            parameters={'operation': 'interface1.op_resumable',
+            parameters={'operation': operation,
                         'operation_kwargs': {
-                            'wait_message': wait_message,
-                            'target_file': target_file
+                            'wait_message': self.wait_message,
+                            'target_file': self.target_file
                         }})
 
+    def _wait_for_log(self, execution):
         self.logger.info('Waiting for operation to start')
         while True:
             logs = self.client.events.list(
                 execution_id=execution.id, include_logs=True)
-            if any(wait_message == log['message'] for log in logs):
+            if any(self.wait_message == log['message'] for log in logs):
                 break
             time.sleep(1)
 
+    def _restart_mgmtworker(self):
         self.logger.info('Stopping mgmtworker')
         self.execute_on_manager('systemctl stop cloudify-mgmtworker')
-        self.assertFalse(self.client.node_instances.get(instance_id)
-                         .runtime_properties['resumed'])
 
         self.logger.info('Restarting mgmtworker')
-        self.execute_on_manager('touch {0}'.format(target_file))
+        self.execute_on_manager('touch {0}'.format(self.target_file))
         self.execute_on_manager('systemctl start cloudify-mgmtworker')
 
-        self.logger.info('Waiting for execution to finish')
+    def test_resumable_mgmtworker_op(self):
+        dep = self._create_deployment()
+        instance = self.client.node_instances.list(deployment_id=dep.id)[0]
+        execution = self._start_execution(dep, 'interface1.op_resumable')
+        self._wait_for_log(execution)
+
+        self.assertFalse(self.client.node_instances.get(instance.id)
+                         .runtime_properties['resumed'])
+        self._restart_mgmtworker()
+        self.logger.info('Waiting for the execution to finish')
         while True:
             new_exec = self.client.executions.get(execution.id)
-            if new_exec.status == 'terminated':
-                break
-            time.sleep(1)
-        self.assertTrue(self.client.node_instances.get(instance_id)
+            if new_exec.status == 'started':
+                time.sleep(1)
+                continue
+            self.assertEqual(new_exec.status, 'terminated')
+        self.assertTrue(self.client.node_instances.get(instance.id)
                         .runtime_properties['resumed'])
 
     def test_nonresumable_mgmtworker_op(self):
-        wait_message = 'WAITING FOR FILE'
-        target_file = '/tmp/continue_test'
-        dsl_path = resource("dsl/resumable_mgmtworker.yaml")
-        deployment = self.deploy(dsl_path)
-        instances = self.client.node_instances.list(
-            deployment_id=deployment.id)
-        instance_id = instances[0].id
+        dep = self._create_deployment()
+        instance = self.client.node_instances.list(deployment_id=dep.id)[0]
+        execution = self._start_execution(dep, 'interface1.op_resumable')
+        self._wait_for_log(execution)
 
-        execution = self.execute_workflow(
-            workflow_name='execute_operation',
-            wait_for_execution=False,
-            deployment_id=deployment.id,
-            parameters={'operation': 'interface1.op_nonresumable',
-                        'operation_kwargs': {
-                            'wait_message': wait_message,
-                            'target_file': target_file
-                        }})
-
-        self.logger.info('Waiting for operation to start')
-        while True:
-            logs = self.client.events.list(
-                execution_id=execution.id, include_logs=True)
-            if any(wait_message == log['message'] for log in logs):
-                break
-            time.sleep(1)
-
-        self.logger.info('Stopping mgmtworker')
-        self.execute_on_manager('systemctl stop cloudify-mgmtworker')
-        self.logger.info('Restarting mgmtworker')
-        self.execute_on_manager('touch {0}'.format(target_file))
-        self.execute_on_manager('systemctl start cloudify-mgmtworker')
-
-        self.logger.info('Waiting for execution to finish')
-        import pudb; pu.db  # NOQA
+        self._restart_mgmtworker()
+        self.logger.info('Waiting for the execution to fail')
         while True:
             new_exec = self.client.executions.get(execution.id)
-            if new_exec.status == 'terminated':
-                break
-            time.sleep(1)
-        self.assertFalse(self.client.node_instances.get(instance_id)
+            if new_exec.status == 'started':
+                time.sleep(1)
+                continue
+            self.assertEqual(new_exec.status, 'failed')
+        self.assertFalse(self.client.node_instances.get(instance.id)
                          .runtime_properties['resumed'])
