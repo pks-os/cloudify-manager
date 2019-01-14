a748fcfe650197671568e64f3fcd478a4597404d
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-01-14 06:16:39 +0100

    test

---
+++ tests/integration_tests/tests/agentless_tests/test_resume.py
@@ -0,0 +1,30 @@
+########
+# Copyright (c) 2019 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+from integration_tests import AgentlessTestCase
+from integration_tests.tests.utils import get_resource as resource
+
+
+class TestTaskResume(AgentlessTestCase):
+
+    def test_resumable_mgmtworker_op(self):
+        dsl_path = resource("dsl/resumable_mgmtworker.yaml")
+        deployment = self.deploy(dsl_path)
+        execution = self.execute_workflow(
+            workflow_name='execute_operation',
+            wait_for_execution=True,
+            deployment_id=deployment.id,
+            parameters={'operation': 'interface1.op1'})
+        print execution
